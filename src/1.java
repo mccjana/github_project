ab56839f98ba7295f034cf0a25597190801d1a11
mccjana <mcc.jana@gmx.com>
2025-03-04 15:15:13 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class GitHubProject {
+  public static void main(String[] args) {
+    Random rand = new Random();
+    int num = rand.nextInt(100);
+    System.out.println("Generated random number: " + num);
+  }
+}
